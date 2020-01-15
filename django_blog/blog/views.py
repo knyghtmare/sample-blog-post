fe9e5965bb8e7c95820fdb5c1ca93caa9635924d
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-15 23:17:39 +0600

    commented out dummy data

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
 from django.http import HttpResponse
 
 # dummy data for testing purposes
-
+'''
 posts = [
     {
         'author' : 'John Doe',
@@ -12,18 +12,19 @@ posts = [
         'date_posted' : 'January 16th, 2020'
     },
     {
-        'author' : 'Sadia',
+        'author' : 'A Random Idiot',
         'title'  : 'Blog Post 2',
         'content': 'Contents of whatever blog post this is',
         'date_posted' : 'January 17th, 2020'
     }
 ]
+'''
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     # return HttpResponse('<h1>Blog Homepage</h1>')
     return render(request, 'blog/home.html', context)
